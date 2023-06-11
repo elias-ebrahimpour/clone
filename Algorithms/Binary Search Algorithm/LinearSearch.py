aefdedb6ffb2d4e281f2483f4d8e36574ccac26d
Elias EbrahimPour <e.ebrahimpour@iranserver.com>
2023-06-11 15:55:06 +0330

    change name non-binary to linear search

--- Algorithms/Binary Search Algorithm/LinearSearch.py
+++ Algorithms/Binary Search Algorithm/LinearSearch.py
@@ -1,4 +1,4 @@
-def non_binary_search(number: int, array: list[int]):
+def linear_search(number: int, array: list[int]):
     for item in array:
         if number == item:
             print(item)
@@ -6,7 +6,7 @@ def non_binary_search(number: int, array: list[int]):
 
 
 def main():
-    non_binary_search(9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    linear_search(9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 if __name__ == "__main__":
     main()

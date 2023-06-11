057e0229a72b739068c74fe46312da7a3007b127
Elias EbrahimPour <e.ebrahimpour@iranserver.com>
2023-06-11 15:59:08 +0330

    improve code linear search

--- Algorithms/Binary Search Algorithm/LinearSearch.py
+++ Algorithms/Binary Search Algorithm/LinearSearch.py
@@ -1,12 +1,17 @@
-def linear_search(number: int, array: list[int]):
+def linear_search(number: int, array: list[int])-> bool:
+    """
+    This is an example code for Linear Search in python
+    """
     for item in array:
         if number == item:
-            print(item)
+            return True
+    return False
 
 
 
 def main():
-    linear_search(9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    print(linear_search(9, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))    # Will returns "True"
+    print(linear_search(29, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))   # Will returns "False"
 
 if __name__ == "__main__":
     main()
